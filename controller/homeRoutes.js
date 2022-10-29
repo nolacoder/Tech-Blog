f236e6dabfceae1a2e30a6897bfdd792f7d71002
Jared <jjohns3@tulane.edu>
2022-10-29 11:34:51 -0500

    beginning homeRoutes.js

---
+++ controller/homeRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { User, Comment, Post } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+      const postData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      res.render(' ', { 
+        posts, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
