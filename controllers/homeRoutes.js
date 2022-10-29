1141b3ee4f606cbf3409ae80bf98ce168d53e296
Jared <jjohns3@tulane.edu>
2022-10-29 16:55:04 -0500

    added in necessary packages and created helper file for handlebars rendering

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,21 +4,22 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
-      const postData = await Post.findAll({
-        include: [
-          {
-            model: User,
-            attributes: ['name'],
-          },
-        ],
-      });
+      // const postData = await Post.findAll({
+      //   include: [
+      //     {
+      //       model: User,
+      //       attributes: ['name'],
+      //     },
+      //   ],
+      // });
   
-      const posts = postData.map((post) => post.get({ plain: true }));
+      // const posts = postData.map((post) => post.get({ plain: true }));
   
-      res.render('all-posts', { 
-        posts, 
-        logged_in: req.session.logged_in 
-      });
+      // res.render('all-posts', { 
+      //   posts, 
+      //   logged_in: req.session.logged_in 
+      // });
+      res.render('login')
     } catch (err) {
       res.status(500).json(err);
     }
