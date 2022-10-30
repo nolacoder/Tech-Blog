793a43463d02e6cfb537236f2908d983566cf7fe
Jared <jjohns3@tulane.edu>
2022-10-29 20:00:04 -0500

    starting login and signup js files and minimal css

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,26 +3,61 @@ const { User, Comment, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-    try {
-      // const postData = await Post.findAll({
-      //   include: [
-      //     {
-      //       model: User,
-      //       attributes: ['name'],
-      //     },
-      //   ],
-      // });
-  
-      // const posts = postData.map((post) => post.get({ plain: true }));
-  
-      // res.render('all-posts', { 
-      //   posts, 
-      //   logged_in: req.session.logged_in 
-      // });
-      res.render('login')
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
 
-  module.exports = router;
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('all-posts', {
+      posts,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const postData = await User.findByPk(req.session.userId, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+
+    const posts = postData.get({ plain: true });
+    console.log(posts);
+
+
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('signup');
+});
+
+
+module.exports = router;
